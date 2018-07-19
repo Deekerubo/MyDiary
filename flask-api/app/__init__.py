14577d50a526bc50d6357846e640119cef4af163
Deekerubo <dianakerubo24@gmail.com>
2018-07-20 02:25:17 +0300

    Implementing GET tests

--- flask-api/app/__init__.py
+++ flask-api/app/__init__.py
@@ -1,5 +1,10 @@
 from flask_api import FlaskAPI
+from instance.config import app_config
 
-app = FlaskAPI(__name__)
+def create_app(config_name):
+    app = FlaskAPI(__name__, intstance_relative_config=True)
+    app.config.from_pyfile('config.py')
 
-from app.views import *
+    from app.views import *
+
+    return app
--- flask-api/tests/test.py
+++ flask-api/tests/test.py
@@ -1,7 +1,20 @@
 import unittest
 import os
 import json
+from app import create_app
 
+class Tests(unittest.TestCase):
+    """Test for entries"""
+
+    def setUp(self):
+        self.app = create_app(config_name="testing")
+        self.route_get_entries = '/api/v1/entries'
+        self.route_index = '/api/v1'
+        self.client = self.app.test_client
+
+    def test_index(self):
+        result = self.client.get(self.route_index)
+        self.assertEqual(result.status_code, 200)
 
 
 
