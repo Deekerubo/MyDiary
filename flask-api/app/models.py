fc3d6504781d587139e40501642831638a95d947
Deekerubo <dianakerubo24@gmail.com>
2018-07-21 00:49:40 +0300

    Implementing API endpoints

--- flask-api/app/models.py
+++ flask-api/app/models.py
@@ -7,7 +7,11 @@ class Models(object):
     def add(self, title, content):
         self.title = title
         self.content = content
+        number_of_entries = 0
+        for i in range(len(self.Entries)):
+            number_of_entries += 1
         an_entry = {}
+        an_entry['id'] = number_of_entries
         an_entry['title'] = self.title
         an_entry['content'] = self.content
 
--- flask-api/app/views.py
+++ flask-api/app/views.py
@@ -1,7 +1,8 @@
 from flask import Flask
-from app import app
 from flask import jsonify, request
 from app.models import Models
+
+app = Flask(__name__)
 
 # create an instance of models class
 models = Models()
@@ -27,3 +28,22 @@ def adding():
 @app.route('/api/v1/entries' ,methods=["GET"])
 def get_entries():
     return jsonify(models.all_entries()), 200
+
+@app.route('/api/v1/entries/<int:entryId>')
+def get_one_entry(entryId):
+    entries_in_model = models.all_entries()
+    for i in entries_in_model:
+        if i['id'] == entryId:
+            return jsonify(i)
+
+@app.route('/api/v1/entries/<int:entryId>', methods=['PUT'])
+def edit_one_entry(entryId):
+    entries_in_model = models.all_entries()
+    for i in entries_in_model:
+        if i['id'] == entryId:
+            data = request.get_json()
+            title = data['title']
+            content = data['content']
+            i['title'] = title
+            i['content'] = content
+            return jsonify({"title":title, "content":content})
