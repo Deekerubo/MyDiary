5a612046f43424d7e43cdafd8da33276c2556691
Deekerubo <dianakerubo24@gmail.com>
2018-07-27 07:32:17 +0300

    Coonecting to database

--- flask-api/app/dbcreated.py
+++ flask-api/app/dbcreated.py
@@ -1,2 +1,60 @@
-import os 
-from psy
+import os
+from psycopg2 import connect
+
+def connect_to_db():
+    ''' Function to create a connection to the right database'''
+    if config=='testing':
+        db_name = os.getenv('TEST_DB')
+    else:
+        db_name = os.getenv('DB_NAME')
+    
+    host = os.getenv('DB_HOST')
+    user = os.getenv('DB_USERNAME')
+    password = os.getenv('DB_PASSWORD')
+
+    return connect(
+        database=db_name,
+        host=host,
+        user=user,
+        password=password
+        )
+
+def create_users_table(cur):
+    """Fuction to create a table for users"""
+    cur.execute(
+        """CREATE TABLE users(
+            id serial PRIMARY KEY,
+            username VARCHAR NOT NULL UNIQUE,
+            email VARCHAR NOT NULL UNIQUE,
+            password VARCHAR NOT NULL);""")
+
+def create_entries_table(cur):
+    """Function to create a table for entries"""
+    cur.execute(
+        """CREATE TABLE entries(
+            id serial,
+            user_id INTEGER NOT NULL,
+            title VARCHAR NOT NULL,
+            description VARCHAR NOT NULL,
+            created_at timestamp NOT NULL,
+            last_modified timestamp NOT NULL,
+            PRIMARY KEY (user_id , id),
+            FOREIGN KEY (user_id) REFERENCES users (id));""")
+
+def main(config=None):
+    conn = connect_to_db(config=config)
+    conn.set_session(autocommit=True)
+    cur = conn.cursor()
+    cur.execute("""DROP TABLE IF EXISTS users CASCADE""" )
+    cur.execute("""DROP TABLE IF EXISTS entries CASCADE""" )
+    
+    create_users_table(cur)
+    create_entries_table(cur)
+
+    cur.close()
+    conn.commit()
+    conn.close()
+    print('Database created successfully')
+
+if __name__ == '__main__':
+    main()
--- flask-api/run.py
+++ flask-api/run.py
@@ -2,7 +2,4 @@ import os
 from app import app
 
 if __name__== "__main__":
-    
-    config_name = os.getenv('FLASK_ENV')
-    app = create_app(config_name)
     app.run(debug=True)
